b6ad9803c3eed12ac00e66ae8ddef8a46f570d8f
Stefan-George Cojocaru <cojoc07@gmail.com>
2020-08-02 18:17:25 +0300

    added routes for tutorials

--- routes/default-router.js
+++ routes/default-router.js
@@ -2,22 +2,31 @@ const express = require('express')
 
 const MovieCtrl = require('../controllers/movie-ctrl')
 const UserCtrl = require('../controllers/user-controller')
+const TutorialCtrl = require('../controllers/tutorial-ctrl')
 
 const router = express.Router()
 
+//movies
 router.get('/movies', MovieCtrl.getMovies)
 router.get('/movie/:id', MovieCtrl.getMovieById)
 router.post('/movie', MovieCtrl.createMovie)
 router.put('/movie/:id', MovieCtrl.updateMovie)
 router.delete('/movie/:id', MovieCtrl.deleteMovie)
 
+//users
 router.get('/users', UserCtrl.getUsers)
+
+//tutorials
+router.get('/tutorials', TutorialCtrl.getTutorials)
+router.get('/tutorial/:id', TutorialCtrl.getTutorialById)
+router.post('/tutorial', TutorialCtrl.createTutorial)
+router.put('/tutorial/:id', TutorialCtrl.updateTutorial)
+router.delete('/tutorial/:id', TutorialCtrl.deleteTutorial)
+
 router.get('/test', function(req, res){
     res.send("ai nimerit pe o pagina de test!")
 })
 
-router.get('/test2', function(req, res){
-    res.send("Inca un test din git")
-})
+
 
 module.exports = router
